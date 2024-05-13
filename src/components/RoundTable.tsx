f149943cec5e4be4ad6771763bf828485862c032
Zack <71916918+zakki-miilo@users.noreply.github.com>
2024-05-13 18:41:36 +1200

    table fit for mobile

--- src/components/RoundTable.tsx
+++ src/components/RoundTable.tsx
@@ -18,39 +18,42 @@ const RoundTable: React.FC<RoundTableProps> = ({
   currentRound,
 }) => {
   return (
-    <table className="table table-dark">
-      <thead>
-        <tr>
-          <th className="header-cell">Round</th>
-          <th className="header-cell">Goal</th>
-          <th className="header-cell">Cards Dealt</th>
-          {players.map((player, index) => (
-            <th key={index} className="header-cell">
-              {player.name}
-            </th>
+    <div className="table-responsive">
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th className="header-cell">Round</th>
+            <th className="header-cell">Goal</th>
+            <th className="header-cell">Cards Dealt</th>
+            {players.map((player, index) => (
+              <th key={index} className="header-cell">
+                {player.name}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rounds.map((round, index) => (
+            <React.Fragment key={round.number}>
+              <tr className={index === currentRound ? "table-warning" : ""}>
+                <td>{round.number}</td>
+                <td>{round.goal}</td>
+                <td>{round.cards}</td>
+                {round.points.slice(0, players.length).map((point, index) => (
+                  <td key={index}>{point}</td>
+                ))}
+              </tr>
+              <tr className={index === currentRound ? "table-warning" : ""}>
+                <td colSpan={3}></td>
+                {round.buys.slice(0, players.length).map((buy, index) => (
+                  <td key={index}>{buy} buy(s)</td>
+                ))}
+              </tr>
+            </React.Fragment>
           ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rounds.map((round, index) => (
-          <React.Fragment key={round.number}>
-            <tr className={index === currentRound ? "table-warning" : ""}>
-              <td rowSpan={2}>{round.number}</td>
-              <td rowSpan={2}>{round.goal}</td>
-              <td rowSpan={2}>{round.cards}</td>
-              {round.points.slice(0, players.length).map((point, index) => (
-                <td key={index}>{point}</td>
-              ))}
-            </tr>
-            <tr className={index === currentRound ? "table-warning" : ""}>
-              {round.buys.slice(0, players.length).map((buy, index) => (
-                <td key={index}>{buy} buy(s)</td>
-              ))}
-            </tr>
-          </React.Fragment>
-        ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
